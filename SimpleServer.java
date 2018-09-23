1a26f7cc347f2a0ae20bd6adf065a06a2695fd68
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2018-09-23 17:34:18 -0600

    Add files via upload
    URLDecoder instead of replacing escaped chars in strings, parse headers, handle HEAD requests, added appropriate content-type headers to GET and HEAD requests, passed inputstream outputstream rather than reading to write.

---
+++ SimpleServer.java
@@ -0,0 +1,79 @@
+package ws;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+//TODO: SecurityManager, set up security policy and checkaccept from host/port
+public class SimpleServer implements Runnable{
+	private static final int DEFAULT_PORT = 8080;
+	private static final int MAX_THREADS = 4;
+	private ServerSocket hsock;
+	private ExecutorService servicer;
+	
+	public SimpleServer() {
+		this(DEFAULT_PORT);
+	}
+	
+	public SimpleServer(int port){
+		try {
+			hsock = new ServerSocket(port);
+			//Creates a thread pool that reuses a fixed number of threads operating off a shared unbounded queue.
+			servicer = Executors.newFixedThreadPool(MAX_THREADS);
+		}
+		catch(IOException ie) {
+			ie.printStackTrace();
+		}
+		finally {
+			try {
+				hsock.close();
+			}
+			catch(IOException lastTry) {
+				lastTry.printStackTrace();
+			}
+		}
+	}
+	
+	public void run() {
+		try {
+			while(true) { //just run forever
+				//Executes the given task sometime in the future.
+				servicer.execute(new ServiceHandler(hsock.accept()));
+			}
+		}
+		catch(IOException ioe) {
+			servicer.shutdown(); //will execute previously submitted tasks before going away
+		}
+		finally {
+			if(!servicer.isShutdown())
+				servicer.shutdownNow(); //prevents waiting tasks from starting and attempts to stop currently executing tasks
+			try {
+				hsock.close();
+			}
+			catch(IOException ie) {
+				ie.printStackTrace();
+			}
+		}
+	}
+}
+
+class ServiceHandler implements Runnable{
+	private final Socket socket;
+	private final HttpRequestParser parser;
+	ServiceHandler(Socket socket){ 
+		this.socket = socket; 
+		parser = new HttpRequestParser(socket);
+	}
+	
+	public void run() {
+		try {
+		    parser.parseRequest();
+		    socket.close();
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+}
