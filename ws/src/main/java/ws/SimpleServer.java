d62caa2ac49d7f3c31a997dc7287d52dc492c51b
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2018-09-24 12:57:41 -0600

    Update SimpleServer.java
    added config file with vars to replace static finals

--- ws/src/main/java/ws/SimpleServer.java
+++ ws/src/main/java/ws/SimpleServer.java
@@ -1,33 +1,84 @@
 package ws;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.concurrent.Executors;
+import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 
 //TODO: SecurityManager, set up security policy and checkaccept from host/port
 public class SimpleServer implements Runnable{
-	//TODO: move these to properties file
-	private static final int DEFAULT_PORT = 8080;
-	private static final int MAX_THREADS = 4;
+	private Properties config = new Properties();
+	private int default_port = -1;
+	private int max_threads = -1;
 	private ServerSocket hsock;
 	private ExecutorService servicer;
 	
 	public SimpleServer() {
-		this(DEFAULT_PORT);
+		this(-1);
 	}
 	
 	public SimpleServer(int port){
 		try {
-			hsock = new ServerSocket(port);
+			InputStream in = null;
+			try {
+				File pfile = new File("./src/main/resources/config.properties");
+				in = new FileInputStream(pfile);
+				config.load(in);
+				in.close();
+			}
+			catch(IOException ioe) {
+				ioe.printStackTrace();
+			}
+			
+			//if we couldn't get the file from the file system, try the classloader
+			if(in == null) {
+				try {
+					String filename = "main/resources/config.properties";
+					ClassLoader cl = getClass().getClassLoader();
+					URL res = Objects.requireNonNull(cl.getResource(filename),"Can't find configuration file " + filename);
+					
+					in = new FileInputStream(res.getFile());
+					config.load(in);
+					in.close();
+				}
+				catch(IOException ioex) {
+					ioex.printStackTrace();
+				}
+			}
+				
+			if(config.containsKey("default_port"))
+				default_port = Integer.parseInt(config.getProperty("default_port"));
+			
+			if(config.containsKey("max_threads"))
+				max_threads = Integer.parseInt(config.getProperty("max_threads"));
+				
+			if(default_port < 0 || max_threads < 0)
+				throw new Exception("Invalid configuration. Please update application configuration file");
+			
+			int use_port = 0;
+			if(port < 0)
+				use_port = default_port;
+			else
+				use_port = port;
+			
+			hsock = new ServerSocket(use_port);
 			//Creates a thread pool that reuses a fixed number of threads operating off a shared unbounded queue.
-			servicer = Executors.newFixedThreadPool(MAX_THREADS);
+			servicer = Executors.newFixedThreadPool(max_threads);
 		}
 		catch(IOException ie) {
 			try {hsock.close();}
 			catch(Exception e) {}
 			ie.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
@@ -39,6 +90,7 @@ public class SimpleServer implements Runnable{
 			}
 		}
 		catch(IOException ioe) {
+			ioe.printStackTrace();
 			servicer.shutdown(); //will execute previously submitted tasks before going away
 		}
 		finally {
