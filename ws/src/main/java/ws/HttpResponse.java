80714b58f0fc37562140b42ec44c861cb9e20c1d
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2018-09-25 21:50:03 -0600

    added charset support

--- ws/src/main/java/ws/HttpResponse.java
+++ ws/src/main/java/ws/HttpResponse.java
@@ -10,9 +10,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.net.Socket;
-import java.net.URL;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -21,52 +18,40 @@ import javax.activation.MimetypesFileTypeMap;
 public class HttpResponse {
 	private Vector<String> headers = new Vector<String>();
 	private StatusCode codes = new StatusCode();
-	private Socket socket = null;
+	private OutputStream socketOut = null;
 	private File contentFile = null;
 	
-	private Properties config = new Properties();
-	private String fileroot = null;
+	private static Properties config = new Properties();
+	private static String fileroot = null;
 	
-	public HttpResponse(Socket socket) {
+	protected String charset = "ISO-8859-1";
+	
+	static {
+		InputStream in = null;
 		try {
-			InputStream in = null;
-			try {
-				File pfile = new File("./src/main/resources/config.properties");
-				in = new FileInputStream(pfile);
-				config.load(in);
-				in.close();
-			}
-			catch(IOException ioe) {
-				ioe.printStackTrace();
-			}
-			
-			//if we couldn't get the file from the file system, try the classloader
-			if(in == null) {
-				try {
-					String filename = "main/resources/config.properties";
-					ClassLoader cl = getClass().getClassLoader();
-					URL res = Objects.requireNonNull(cl.getResource(filename),"Can't find configuration file " + filename);
-					
-					in = new FileInputStream(res.getFile());
-					config.load(in);
-					in.close();
-				}
-				catch(IOException ioex) {
-					ioex.printStackTrace();
-				}
-			}
-				
-			if(config.containsKey("fileroot"))
-				fileroot = config.getProperty("fileroot");
-			
-			if(fileroot == null)
-				throw new Exception("Invalid configuration. Please update application configuration file");
+			File pfile = new File("./src/main/resources/config.properties");
+			in = new FileInputStream(pfile);
+			config.load(in);
+			in.close();
 		}
-		catch(Exception e) {
-			e.printStackTrace();
+		catch(IOException ioe) {
+			ioe.printStackTrace();
 		}
-				
-		this.socket = socket;
+			
+		if(config.containsKey("fileroot"))
+			fileroot = config.getProperty("fileroot");
+		
+		if(fileroot == null)
+			throw new ExceptionInInitializerError("Invalid configuration. Please update application configuration file");
+	
+	}
+	
+	public HttpResponse(OutputStream socketOut) {		
+		this.socketOut = socketOut;
+	}
+	
+	public void setEncoding(String charset) {
+		this.charset = charset;
 	}
 	
 	public void getResponse(String fileloc) {
@@ -132,7 +117,7 @@ public class HttpResponse {
 			}
 			
 			out = new BufferedOutputStream(new FileOutputStream(modfileloc));
-			//is.transferTo(out); //TODO: this is blocking because is never gets an EOF
+			//is.transferTo(out); //TODO: this is blocking because is never gets an end of stream
 			byte[] buffer = new byte[4 * 1024];
 			while (is.available() > 0) {
 				int len = is.read(buffer);
@@ -206,13 +191,12 @@ public class HttpResponse {
 		respbuff.append("Connection: close\r\n"); //close connection
         respbuff.append("\r\n"); //end headers
         
-        OutputStream out = null;
         PrintStream ps = null;
         FileInputStream in = null;
         try {
-	        out = socket.getOutputStream();
-	        ps = new PrintStream(out);
+	        ps = new PrintStream(socketOut, true, charset);
 	        ps.print(respbuff.toString()); //this contains headers
+	        ps.flush();
 	        
 	        if(contentFile != null && contentFile.length() > 0) {
 		        in = new FileInputStream(contentFile);
@@ -222,8 +206,8 @@ public class HttpResponse {
 	        ps.flush();
 	        ps.close();
 	        
-	        out.flush();
-	        out.close();
+	        socketOut.flush();
+	        socketOut.close();
         }
         catch(IOException ioe) {
         	ioe.printStackTrace();
@@ -232,8 +216,8 @@ public class HttpResponse {
         	if(ps != null)
         		ps.close();
 	        try {
-		        if(out != null)
-		        	out.close();
+		        if(socketOut != null)
+		        	socketOut.close();
 		        if(in != null)
 		        	in.close();
 	        }
