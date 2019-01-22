6d67276b6daf8db85b0bc425c40efd0caab35bea
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2019-01-22 12:43:16 -0700

    Update HttpRequestParser.java
    fixed a few issues flagged by Find Bugs

--- ws/src/main/java/ws/HttpRequestParser.java
+++ ws/src/main/java/ws/HttpRequestParser.java
@@ -3,8 +3,6 @@ package ws;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
@@ -12,8 +10,10 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Properties;
 import java.util.StringTokenizer;
-import java.util.Vector;
-
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.nio.charset.Charset;
 
 public class HttpRequestParser {
@@ -24,23 +24,42 @@ public class HttpRequestParser {
 	private Hashtable<String,String> headerHash = new Hashtable<String,String>();
 	private String charset = "ISO-8859-1";
 	
+	/*This is a very simple logging implementation of a logging system. At some point it would be wise to move to an abstraction like Apache Commons or SL4J*/
+	private final static Logger LOGGER = Logger.getLogger(SimpleServer.class.getName());
+	private static Handler logFileHandler = null;
+ 	
 	static {
 		InputStream in = null;
 		try {
-			File pfile = new File("./src/main/resources/config.properties");
-			in = new FileInputStream(pfile);
+			in = HttpRequestParser.class.getClassLoader().getResourceAsStream("config.properties");
 			config.load(in);
 			in.close();
 		}
 		catch(IOException ioe) {
 			ioe.printStackTrace();
 		}
+		
+		try {
+			String logdir = "";
+			if(config.containsKey("logdir"))
+				logdir = config.getProperty("logdir");
 			
+			logFileHandler = new FileHandler(logdir + "httpRequestParser.log");
+			LOGGER.addHandler(logFileHandler);
+			logFileHandler.setLevel(Level.ALL);
+			LOGGER.setLevel(Level.ALL);
+		}
+		catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
 		if(config.containsKey("version"))
 			version = config.getProperty("version");
 		
-		if(version == null)
+		if(version == null) {
+			LOGGER.log(Level.CONFIG, "Invalid configuration for " + SimpleServer.class.getName() + " Please update application configuration file.");
 			throw new ExceptionInInitializerError("Invalid configuration. Please update application configuration file");
+		}
 	}
 	
 	public HttpRequestParser(BufferedOutputStream out) {
@@ -48,7 +67,7 @@ public class HttpRequestParser {
 	}
 	
 	public void parseRequest(InputStream is) {
-		Vector<String> headers = new Vector<String>();
+		ArrayList<String> headers = new ArrayList<String>();
 		BufferedInputStream bis = null;
     	String metver = null;
     	try {    		 
@@ -57,7 +76,7 @@ public class HttpRequestParser {
 	 		   BNF, an HTTP/1.1 client MUST NOT preface or follow a request with an
 	 		   extra CRLF.
 	 		   
-	 		   this supports only 1.1. and will fail on preceding CRLFs
+	 		   this web server supports only 1.1. and will fail on preceding CRLFs
 	 		*/
 	     	
 	     	//rfc2616: Request-Line   = Method SP Request-URI SP HTTP-Version CRLF
@@ -66,15 +85,15 @@ public class HttpRequestParser {
 	     	metver = URLDecoder.decode(metver, charset); //remove escaped characters
     	}
     	catch(IOException ee) {
-    		ee.printStackTrace();
-    		responseObj.sendResponse("400");
+    		LOGGER.logp(Level.WARNING, this.getClass().getName(), ee.getStackTrace()[0].getMethodName(), "IOException caught", (Throwable)ee);
+    		responseObj.sendResponse("400", false);
     		return;
     	}
     	
     	String method = null;
      	String location = null;
      	String httpver = null;
-    	if(metver != null && metver.length() > 0) {
+    	if(metver.length() > 0) {
     		ArrayList<String> tokens = new ArrayList<String>();
     		StringTokenizer tokenizer = new StringTokenizer(metver, " ");
     	    while (tokenizer.hasMoreElements()) {
@@ -89,41 +108,49 @@ public class HttpRequestParser {
 
 	    		//there must be all three in a request
 		    	if(method == null || location == null || httpver == null) {
-		    		responseObj.sendResponse("400");
+		    		responseObj.sendResponse("400", false);
 		    		return;
 		    	}
 		    	if(!httpver.equals(version)) {
-		    		responseObj.sendResponse("505");
+		    		responseObj.sendResponse("505", false);
 		    		return;
 		    	}
     	    }
     	    catch(IndexOutOfBoundsException bound) {
-    	    	responseObj.sendResponse("400");
+    	    	LOGGER.logp(Level.WARNING, this.getClass().getName(), bound.getStackTrace()[0].getMethodName(), "IndexOutOfBoundsException caught", (Throwable)bound);
+    	    	responseObj.sendResponse("400", false);
     	    }
     	}
     	else {
-    		responseObj.sendResponse("400"); //possibly a preceding CRLF in request
+    		responseObj.sendResponse("400", false); //possibly a preceding CRLF in request
     		return;
     	}
     	
     	try {
 	    	//parse headers
     		String headstr = stringifyBinaryLine(bis);
-    		while(headstr != null && headstr.length() > 1) {
+    		while(headstr.length() > 1) {
 	    		headers.add(headstr);
 	    		headstr = stringifyBinaryLine(bis);
 	    	}
 	    	if(headers.size() > 0)
 	    		parseHeaders(headers);
+	    		    	
+	    	//this is a redundant check, but just to be sure
+	    	if(method == null) {
+	    		LOGGER.logp(Level.WARNING, this.getClass().getName(), "parseRequest", "Http method was empty");
+	    		responseObj.sendResponse("400", false); //possibly a preceding CRLF in request
+	    		return;
+	    	}
 	    	
-	    	String decodeloc = URLDecoder.decode(location, charset).replaceAll(" ",""); //decode url, ie: remove escaped spaces and remove whitespace
+	    	String decodeloc = URLDecoder.decode(location, charset).replaceAll(" ",""); //decode url, ie: remove escaped spaces and remove whitespace	    	
 	    	if(method.equalsIgnoreCase("GET")) {
 	    		//check headerHash for if-none-match
 	    		String match = null;
 	    		if(headerHash.containsKey("If-None-Match"))
 	    			match = headerHash.get("If-None-Match");
 	    		
-		    	responseObj.getResponse(decodeloc, match);
+		    	responseObj.getResponse(decodeloc, match, false);
 		    	return;
 		    }
 		    else if (method.equalsIgnoreCase("PUT")) {
@@ -141,18 +168,17 @@ public class HttpRequestParser {
 		    }
     	}
     	catch(Exception e) {
-    		e.printStackTrace();
+    		LOGGER.logp(Level.WARNING, this.getClass().getName(), e.getStackTrace()[0].getMethodName(), "Exception caught", (Throwable)e);
     	}
     	finally {
     		try {
-    			if(bis != null)
-    				bis.close();
+    			bis.close();
     		}
     		catch(IOException sigh) {
-    			sigh.printStackTrace();
+    			LOGGER.logp(Level.WARNING, this.getClass().getName(), sigh.getStackTrace()[0].getMethodName(), "BufferedInputStream failed to close on exception.");
     		}
     	}
-    	responseObj.sendResponse("505"); //we should never get here
+    	responseObj.sendResponse("505", false); //we should never get here
     	return;
 	}
 	
@@ -170,7 +196,7 @@ public class HttpRequestParser {
 		 content-type field if they support that charset, rather than the
 		 recipient's preference, when initially displaying a document. See
 		 section 3.7.1.*/
-	private void parseHeaders(Vector<String> requests) {
+	private void parseHeaders(ArrayList<String> requests) {
 		String encodingKey = "accept-charset";
 		for(String r : requests) {
 			int colidx = r.indexOf(":");
@@ -231,10 +257,10 @@ public class HttpRequestParser {
 		If no "*" is present in an Accept-Charset field, then all character sets not explicitly mentioned get a quality value of 0, except for ISO-8859-1, which gets
 		a quality value of 1 if not explicitly mentioned.
 		Accept-Charset: iso-8859-5, unicode-1-1;q=0.8*/
-		if(charsetHeader.indexOf("*") > 0) 
+		if(charsetHeader.indexOf("*") > -1) 
 			return; //using ISO-8859-1, ignoring quality value(s)
 		
-		if(charsetHeader.indexOf(",") > 0){ //multiple possible charsets
+		if(charsetHeader.indexOf(",") > -1){ //multiple possible charsets
 			Double hiq = -1.0;
 			String hics = null;
 			StringTokenizer chop = new StringTokenizer(charsetHeader,",");
@@ -257,8 +283,8 @@ public class HttpRequestParser {
     	        		}
     	        	}
     	        	catch(Exception e) {
-    	        		e.printStackTrace();
-    	        		responseObj.sendResponse("406"); //according to rfc2616
+    	        		LOGGER.logp(Level.WARNING, this.getClass().getName(), e.getStackTrace()[0].getMethodName(), "Exception caught", (Throwable)e);
+    	        		responseObj.sendResponse("406", false); //according to rfc2616
     	        	}
 	    	    }
     	        else { //no quality score, defaults to 1
@@ -269,12 +295,12 @@ public class HttpRequestParser {
     	        }
     	    }
     	    if(hics == null)
-    	    	responseObj.sendResponse("406"); //according to rfc2616
+    	    	responseObj.sendResponse("406", false); //according to rfc2616
     	    else
     	    	charset = hics;			
 		}
 		else if(!Charset.isSupported(charsetHeader))
-			responseObj.sendResponse("406"); //according to rfc2616
+			responseObj.sendResponse("406", false); //according to rfc2616
 		else
 			charset = charsetHeader; //charsetHeader is one value and is supported
 		
