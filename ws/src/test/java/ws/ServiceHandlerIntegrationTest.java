b686a39aac693476d90a3f6616d409c4ff42e94b
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2019-02-13 12:24:48 -0700

    Update ServiceHandlerIntegrationTest.java
    description 2

--- ws/src/test/java/ws/ServiceHandlerIntegrationTest.java
+++ ws/src/test/java/ws/ServiceHandlerIntegrationTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-/*Standalone that tests character encoding, GET, PUT, and HEAD requests.
- Relies on files from resources: testfile.txt
- Writes putfile.html to configured location*/
+/*Standalone that tests ServiceHandler construction*/
 
 public class ServiceHandlerIntegrationTest {
 	private static int PORT = 8081;
