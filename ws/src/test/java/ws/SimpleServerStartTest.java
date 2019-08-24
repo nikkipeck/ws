ce0caa38169f7168b291502eea90213fce0f039d
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2019-08-24 14:13:21 -0600

    removed timeout message
    The socket timeout was inappropriately set on the server socket. Since it is now on the client socket and the server has no timeout, I removed the message.

--- ws/src/test/java/ws/SimpleServerStartTest.java
+++ ws/src/test/java/ws/SimpleServerStartTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.net.SocketTimeoutException;
-/*Standalone test that will start a server on port 8081 and handle requests. Will timeout after configured accept_timeout*/
+/*Standalone test that will start a server on port 8081 and handle requests*/
 public class SimpleServerStartTest {
 	public static final int PORT = 8081;
 	
