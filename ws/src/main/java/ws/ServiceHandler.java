615741aa48859f61691ff3026aea64b5f2994f4d
nikkipeck <42502655+nikkipeck@users.noreply.github.com>
2019-08-24 14:35:53 -0600

    several updates
    intellij suggestions
    renaming sockets and streams to avoid socket closing problems

--- ws/src/main/java/ws/ServiceHandler.java
+++ ws/src/main/java/ws/ServiceHandler.java
@@ -11,20 +11,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ServiceHandler implements Runnable {
-	private final Socket socket;
-	private HttpRequestParser parser = null;
+	private final Socket handlerSocket;
+	private HttpRequestParser parser;
 	
 	/*This is a very simple logging implementation of a logging system. At some point it would be wise to move to an abstraction like Apache Commons or SL4J*/
 	private final static Logger LOGGER = Logger.getLogger(SimpleServer.class.getName());
-	private Handler logFileHandler = null;
-	
+
 	ServiceHandler(Socket socket){
 		Properties config = new Properties();
-		InputStream in = null;
 		try {
-			in = ServiceHandler.class.getClassLoader().getResourceAsStream("config.properties");
-			config.load(in);
-			in.close();
+			InputStream in = ServiceHandler.class.getClassLoader().getResourceAsStream("config.properties");
+			if(in != null) {
+				config.load(in);
+				in.close();
+			}
 		}
 		catch(IOException ioe) {
 			ioe.printStackTrace();
@@ -35,32 +35,31 @@ public class ServiceHandler implements Runnable {
 			if(config.containsKey("logdir"))
 				logdir = config.getProperty("logdir");
 			
-			logFileHandler = new FileHandler(logdir + "serviceHandler.log");
+			Handler logFileHandler = new FileHandler(logdir + "serviceHandler.log");
 			LOGGER.addHandler(logFileHandler);
 			logFileHandler.setLevel(Level.ALL);
 			LOGGER.setLevel(Level.ALL);
 		}
 		catch(IOException ioe) {
 			ioe.printStackTrace();
-		}		
-		
-		this.socket = socket; 
+		}
+
+		handlerSocket = socket;
 		try {
-			parser = new HttpRequestParser(new BufferedOutputStream(socket.getOutputStream(), 4096));
+			parser = new HttpRequestParser(new BufferedOutputStream(handlerSocket.getOutputStream(), 4096));
 		}
 		catch(IOException e) {
-			LOGGER.logp(Level.SEVERE, this.getClass().getName(), e.getStackTrace()[0].getMethodName(), "IOException caught", (Throwable)e);
+			LOGGER.logp(Level.SEVERE, this.getClass().getName(), e.getStackTrace()[0].getMethodName(), "IOException caught", e);
 		}
 	}
 	
 	public void run() {
 		try {
 			if(parser != null)
-				parser.parseRequest(socket.getInputStream());
-			socket.close();
+				parser.parseRequest(handlerSocket.getInputStream()); //this socket is closed before this call. How?
 		}
 		catch(IOException ioe) {
-			LOGGER.logp(Level.WARNING, this.getClass().getName(), ioe.getStackTrace()[0].getMethodName(), "IOException caught", (Throwable)ioe);
+			LOGGER.logp(Level.WARNING, this.getClass().getName(), ioe.getStackTrace()[0].getMethodName(), "IOException caught", ioe);
 		}
 	}
 }
